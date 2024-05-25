6aab81a957eab450cb239e5d586cea5b3beb3714
FatimaUrpi <fc200509fatima.chavez09@gmail.com>
2024-05-25 13:32:41 -0500

    SimulacroBackend

--- src/main/java/com/centroinformacion/service/RevistaService.java
+++ src/main/java/com/centroinformacion/service/RevistaService.java
@@ -20,4 +20,11 @@ public interface RevistaService {
 	public abstract List<Revista> listaRevistaPorNombreIgualActualiza(String nombre, int idRevista);
 	
 	
+	
+	
+	//para la consulta PC3
+		public abstract List<Revista> listaCompleja(String nombre, String frecuencia, Date fecIni, Date fecFin, int estado,	int idPais,	int idTipo);
+	
+	
+	
 }
